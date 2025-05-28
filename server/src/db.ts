6996cdc955877d4a3420f4c1573943149ea0f2d5
maxp7 <podlughiy.maxim@gmail.com>
2025-05-28 15:25:20 +0200

    DB credentials fix

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,12 +1,12 @@
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config(); // Loads .env in local only
 
 const pool = new Pool({
-  connectionString: 'postgresql://postgres:4Ctfp%26zjLP-UpE%3F@db.popwyadrdgulohlzjajf.supabase.co:5432/postgres',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
+    rejectUnauthorized: false, // Needed for most hosted Postgres (like Render)
   },
 });
 
