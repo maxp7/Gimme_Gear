c32b07385744a96b3a7d8d7be0aa52cda0db2146
Alina Wallstein <alinawallstein8@gmail.com>
2025-05-30 14:17:31 +0200

    2 more categories and svg icons

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -10,14 +10,24 @@ export default function HomePage() {
       <div className="flex items-center justify-center min-h-screen">
   <div className="w-[700px] grid grid-cols-2 grid-rows-2 gap-4">
     <Categorie
-      imgSrc="/laptop.jpg"
+      imgSrc="/laptop.svg"
       altText="Laptops"
       categoryName="Laptops"
     />
-    <Categorie
-      imgSrc="/vr.jpg"
+     <Categorie
+      imgSrc="/vr.svg"
       altText="VR Headsets"
       categoryName="VR Headsets"
+    />
+     <Categorie
+      imgSrc="/equipment.svg"
+      altText="equipment"
+      categoryName="equipment"
+    />
+     <Categorie
+      imgSrc="/audio.svg"
+      altText="Audio & Lighting"
+      categoryName="Audio & Lighting"
     />
   </div>
 </div>
